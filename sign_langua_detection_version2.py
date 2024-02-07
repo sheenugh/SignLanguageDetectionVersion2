e6ab5983b01d112f4d0d2629b011cbc8e791f8bf
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-07 10:57:07 +0800

    for video capture codes

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -6,5 +6,11 @@ import time
 import math
 import numpy
 
+# ========= VIDEO CAPTURE ========
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+offset = 20
+img_size = 300
+counter = 0
 
 # ========= ACTUAL CODES ==========
