b7f5d4f3ef1f62c5ad8d4a7897ddeb6f44b99333
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-07 13:46:31 +0800

    nagwork na

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -5,6 +5,7 @@ from cvzone.HandTrackingModule import HandDetector
 import time
 import math
 import numpy as np
+import mediapipe as mp
 
 # ========= VIDEO CAPTURE ========
 cap = cv2.VideoCapture(0)
@@ -13,7 +14,7 @@ offset = 20
 img_size = 300
 counter = 0
 
-folder = "C:\Users\Myline\Desktop\[PLD] Final Project\SignLanguageDetectionVersion2\Data"
+folder = "SignLanguageDetectionVersion2\Data\Hello"
 
 # ========= ACTUAL CODES ==========
 # - Calling for data collection
@@ -49,8 +50,8 @@ while True :
             height_gap = math.ceil ((img_size-height_cal)/2)
             img_white[height_gap: height_cal + height_gap, : ] = img_resize
         
-        cv2.image_show('ImageCrop', img_crop)
-        cv2.image_show('ImageWhite', img_white)
+        cv2.imshow('ImageCrop', img_crop)
+        cv2.imshow('ImageWhite', img_white)
         
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
