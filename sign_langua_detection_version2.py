b23a5ff672523f197467dbd4e43bfd8b23706f49
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-07 11:14:22 +0800

    aspect ration and changing the variable as descriptive variables baka bumagsak kay sir

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -22,10 +22,18 @@ while True :
     hands , img = detector.findHands(img)
     if hands:
         hand = hands [0]
-        x,y,w,h = hand ['bbox']
+        horizontal_axis, vertical_axis, weight, height = hand ['bbox']
         
         img_white = np.ones((img_size, img_size, 3), np.uint8)* 255
         
-        img_crop = img[y-offset : y + h + offset, x-offset : x + w + offset]
+        img_crop = img[vertical_axis-offset : vertical_axis + height + offset, horizontal_axis-offset : horizontal_axis + weight + offset]
         img_crop_shape = img_crop.shape
+        
+        aspect_ratio = height/weight
+        
+        if aspect_ratio > 1:
+            size_of_image = img_size / height
+            weight_cal = math.ceil(size_of_image*weight)
+            img_resize = cv2.resize(img_crop, (weight_cal, img_size))
+            
         
