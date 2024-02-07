11fec4e3201a19639a39858be0ed9abf991a795a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-07 11:27:03 +0800

    wgap added code

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -22,20 +22,23 @@ while True :
     hands , img = detector.findHands(img)
     if hands:
         hand = hands [0]
-        horizontal_axis, vertical_axis, weight, height = hand ['bbox']
+        horizontal_axis, vertical_axis, width, height = hand ['bbox']
         
         img_white = np.ones((img_size, img_size, 3), np.uint8)* 255
         
-        img_crop = img[vertical_axis-offset : vertical_axis + height + offset, horizontal_axis-offset : horizontal_axis + weight + offset]
+        img_crop = img[vertical_axis-offset : vertical_axis + height + offset, horizontal_axis-offset : horizontal_axis + width + offset]
         img_crop_shape = img_crop.shape
         
-        aspect_ratio = height/weight
+        aspect_ratio = height/width
         
         if aspect_ratio > 1:
             size_of_image = img_size / height
-            weight_cal = math.ceil(size_of_image*weight)
+            weight_cal = math.ceil(size_of_image*width)
             img_resize = cv2.resize(img_crop, (weight_cal, img_size))
             img_resize_shape = img_resize.shape
+            
+            width_gap = math.ceil ((img_size-weight_cal)/2)
+            img_white[: ,width_gap: weight_cal + width_gap]
             
             
         
