68ccad19075be7376062023c841b2a2292ce8176
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-07 11:08:18 +0800

    looping

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -4,7 +4,7 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import time
 import math
-import numpy
+import numpy as np
 
 # ========= VIDEO CAPTURE ========
 cap = cv2.VideoCapture(0)
@@ -13,4 +13,16 @@ offset = 20
 img_size = 300
 counter = 0
 
+folder = "C:\Users\Myline\Desktop\[PLD] Final Project\SignLanguageDetectionVersion2\Data"
+
 # ========= ACTUAL CODES ==========
+# - Calling for data collection
+while True :
+    success, img = cap.read ()
+    hands , img = detector.findHands(img)
+    if hands:
+        hand = hands [0]
+        x,y,w,h = hand ['bbox']
+        
+        
+        
