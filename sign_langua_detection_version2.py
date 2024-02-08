493754044208f7152437f65fc223951419874aaa
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-09 01:00:45 +0800

    data collection: adding photos for letter d,e,f,g of sign language + removing inappropriate photos in e folder

--- sign_langua_detection_version2.py
+++ sign_langua_detection_version2.py
@@ -15,7 +15,7 @@ offset = 20
 img_size = 200
 counter = 0
 
-folder = "Data\Letters\Letter C"   
+folder = "Data\Letters\Letter G"   
 
 # ========= ACTUAL CODES ==========
 # - Calling for data collection
